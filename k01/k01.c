8a13515eeaa0efe6a55f13278b6ae29a2e66f188
35319021 <0510satomaru@gmail.com>
2020-04-23 00:14:13 +0900

    k01

--- k01/k01.c
+++ k01/k01.c
@@ -3,15 +3,19 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave,int i)
+{return ((i-1)*ave)/i + val/i;}
+extern double var_online(double val, double save,int i)
+{return (((i-1)*save)i+pow(val,2)/i)-pow(((i-1)*ave/i)+(val/i));}
 
 int main(void)
 {
-    double val;
+    double val,ave,var,save;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+int i;
+i=0;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,7 +30,12 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
+        i++;
+        ave=ave_online(val,ave,i);
+        var=var_onlaine(val,ave,save,i);
 
+
+save=var_online(save,pow(val,2),i);
 
     
 
@@ -38,6 +47,10 @@ int main(void)
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
+printf("ave=%1f\n",ave);
+printf("var=%1f\n",var);
+
+    
 
 
     return 0;
